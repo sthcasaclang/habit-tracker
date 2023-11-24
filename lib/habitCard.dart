3e0efb53defb75ee1b4cbda96da0b5049028c4e0
Seth Casaclang <147123476+sthcasaclang@users.noreply.github.com>
2023-11-24 14:49:57 +0800

    Added Habit Slider

--- lib/habitCard.dart
+++ lib/habitCard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HabitCard extends StatelessWidget {
   final String habitName;
@@ -10,31 +11,69 @@ class HabitCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 6, right: 6, bottom: 1),
-      child: Container(
-        margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-        height: 80,
-        width: 360,
-        child: Expanded(
-          child: ElevatedButton(
-            onPressed: () {
-              print("");
-            },
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(habitName,
-                  textAlign: TextAlign.left,
-                  style: GoogleFonts.ubuntu(
-                    textStyle: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 20,
+      child: Slidable(
+        endActionPane: ActionPane(motion: const ScrollMotion(), children: [
+          SlidableAction(
+            padding: EdgeInsets.all(10),
+            borderRadius: BorderRadius.circular(15),
+            onPressed: (context) {},
+            backgroundColor: Colors.grey,
+            foregroundColor: Colors.white,
+            icon: Icons.restore,
+            //label: 'Revert',
+          ),
+          SlidableAction(
+            borderRadius: BorderRadius.circular(15),
+            onPressed: (context) {},
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            icon: Icons.keyboard_double_arrow_right_rounded,
+            //label: 'Skip',
+          )
+        ]),
+        child: Padding(
+          padding: EdgeInsets.all(5),
+          child: Expanded(
+            child: ElevatedButton(
+              onPressed: () {
+                print("");
+              },
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(35),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(habitName,
+                          textAlign: TextAlign.left,
+                          style: GoogleFonts.ubuntu(
+                            textStyle: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20,
+                            ),
+                          )),
                     ),
-                  )),
-            ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(255, 255, 255, 255),
-              shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(10.0),
+                  ),
+                  Spacer(),
+                  SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.black,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(10.0),
+                ),
               ),
             ),
           ),
---
+++ lib/utilities/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const grey = Color(0xff808080);
+  static const black = Color.fromARGB(255, 0, 0, 0);
+}
